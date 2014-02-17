3c1d0533e6ac2edc83280d197b45b40725d7f067
Yin <yinxiaoyu888@gmail.com>
2014-02-17 18:19:53 +0800

    Initial Commit

---
+++ Nerdfeed/NerdfeedAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NerdfeedAppDelegate.h
+//  Nerdfeed
+//
+//  Created by Yin on 14-2-17.
+//  Copyright (c) 2014年 Jack Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NerdfeedAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
